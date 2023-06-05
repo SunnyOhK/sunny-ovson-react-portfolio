ef50ee36c9429bd4bee749445675c2c84eac3451
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-06-05 16:55:26 -0500

    testing out mui components

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+// import About from './components/About/About';
+// import Contact from './components/Contact/Contact';
+// import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+// import Projects from './components/Projects/Projects';
+// import Resume from './components/Resume/Resume';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Header /> */}
+    </div>
+  )
+};
+
+export default App;
